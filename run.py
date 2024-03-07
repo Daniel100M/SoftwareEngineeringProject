35926b44999b5ddcef1bdd4fdd9f42c74b7287a6
MichalRuthElisha <91603335+BabyBlue555@users.noreply.github.com>
2024-03-07 16:25:37 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+from flaskblog import app
+
+if __name__ == '__main__':
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://db_project_9fv3_user:6aWLVrhvHaNESHX780JNRTo2N5Z98yIB@dpg-cnj0qned3nmc73fmsbf0-a.oregon-postgres.render.com/db_project_9fv3'
+    app.run(debug=True)
